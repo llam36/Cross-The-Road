c1b6d616a72cbc21961505d6ca5a06d3afede144
dajami3 <dajami3@gatech.edu>
2023-03-29 22:06:32 -0400

    Add the score display

--- app/src/main/java/com/example/sprint_2/GameOverScreen.java
+++ app/src/main/java/com/example/sprint_2/GameOverScreen.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,18 +13,23 @@ public class GameOverScreen extends AppCompatActivity {
     private Button exitButton;
     private Button restartButton;
 
+    private TextView totalScoreLabel;
+    private Player player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameover_screen);
         exitButton = (Button) findViewById(R.id.exit);
         restartButton = (Button) findViewById(R.id.restart);
+        totalScoreLabel = (TextView) findViewById(R.id.totalScoreLabel);
+        String score = String.valueOf(player.getScore());
+        totalScoreLabel.setText(score);
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent send = new Intent(GameOverScreen.this, ConfigScreen.class);
                 startActivity(send);
-
             }
         });
         exitButton.setOnClickListener(new View.OnClickListener() {
